21f43275591159ae9b3963a4e4dd9c723cddb305
Boriz81 <blitzboriz@gmail.com>
2026-05-12 00:03:22 +0900

    11.05.2026

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-    printf("=== АНГЛО-РУССКИЙ ПРАКТИКУМ ===\n");
-    printf("Версия 0.1\n");
-    printf("Программа запущена успешно!\n");
+int main()
+{
+
+    while (true)
+    {
+        printf("=== АНГЛО-РУССКИЙ ПРАКТИКУМ ===\n");
+        printf("Версия 0.1\n");
+        printf("Программа запущена успешно!\n");
+        printf("МЕНЮ:\n");
+        printf("1. Учить слова\n");
+        printf("2. Добавить слово\n");
+        printf("3. Выйти\n");
+    }
 
     return 0;
 }
