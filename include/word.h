89f76feeeca85377ce24af41ba127c3350110956
Boriz81 <blitzboriz@gmail.com>
2026-08-21 14:11:52 +0900

    21.08.26 14:11

--- include/word.h
+++ include/word.h
@@ -2,4 +2,14 @@
 #define WORD_H
 
 // Структура для хранения слова
+typedef struct {
+    int id;
+    char english[50];
+    char russian[100];
+} Word;
 
+// Объявления функций (прототипы)
+void init_word(Word *w, int id, const char *eng, const char *rus);
+void print_word(const Word *w);
+
+#endif
--- src/word.c
+++ src/word.c
@@ -1,1 +1,18 @@
-word
+#include "../include/word.h"
+#include <stdio.h>
+#include <string.h>
+
+// Реализация функции инициализации слова
+void init_word(Word *w, int id, const char *eng, const char *rus) {
+    w->id = id;
+    strcpy(w->english, eng);
+    strcpy(w->russian, rus);
+}
+
+// Реализация функции вывода слова
+void print_word(const Word *w) {
+    printf("Слово %d\n", w->id);
+    printf("Английский: %s\n", w->english);
+    printf("Русский: %s\n", w->russian);
+    printf("---\n");
+}
